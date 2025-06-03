d9ec50cfa8def3e1a22a5fea9fcc989d9a65e17a
arlissondiogo <arlisson.silva@aluno.uepb.edu.br>
2025-06-03 12:52:11 -0300

    fix: removido bug que fazia o tempo de execução e descanso ficarem com numeros absurdos

--- frontend/src/Pages/AddWorkout/AddWorkout.jsx
+++ frontend/src/Pages/AddWorkout/AddWorkout.jsx
@@ -40,8 +40,17 @@ function AddWorkout() {
 
   const timeToSeconds = (timeString) => {
     if (!timeString) return null;
-    const [hours, minutes] = timeString.split(":").map(Number);
-    return hours * 3600 + minutes * 60;
+    const parts = timeString.split(":").map(Number);
+
+    if (parts.length === 2) {
+      const [minutes, seconds] = parts;
+      return minutes * 60 + seconds;
+    } else if (parts.length === 3) {
+      const [hours, minutes, seconds] = parts;
+      return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    return null;
   };
 
   const validateForm = () => {
